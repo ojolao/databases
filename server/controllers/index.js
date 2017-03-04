6d17b1a59151c19134bddbc658f858db3c592b80
ojolao <ojolao@gmail.com>
2017-03-03 20:06:02 -0800

    Defined our sql tables in multi table schema.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,28 @@
 var models = require('../models');
+var mysql = require('mysql');
+
+var sendResponse = function (response, data, statusCode) {
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify(data));
+};
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      if (req.url === '/classes/messages' || req.url === '/classes/messages?order=-createdAt') {
+        statusCode = 200;
+        sendResponse(res); //'messages from our mysql database', statusCode);
+      }
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      statusCode = 201;
+      request.on('data', function(data) {
+        var parsedBody = JSON.parse(data);
+        //'mysql database messages table'.messages.results.push(parsedBody);
+        sendResponse(response, parsedBody, statusCode);
+      });
+    } // a function which handles posting a message to the database
   },
 
   users: {
--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,24 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE rooms (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  content TEXT NOT NULL, 
+  user INTEGER,
+  room INTEGER,
+  FOREIGN KEY(user) REFERENCES users(id),
+  FOREIGN KEY(room) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
